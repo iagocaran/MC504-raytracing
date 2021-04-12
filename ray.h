2da46c2e5c7ab6d204e4e2151375a3669e57f8d7
caah06 <camilabrito.012@gmail.com>
2021-04-12 15:03:00 -0300

    Add files via upload

---
+++ ray.h
@@ -0,0 +1,30 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+typedef struct ray {
+		point3 orig;
+        vec3 dir;
+
+        ray();
+        ray(ray &r,const point3& origin, const vec3& direction);
+        point3 origin(ray &r);
+        vec3 direction(ray &r);
+        point3 at(ray &r, double t);
+};
+ray() {}
+
+ray(ray &r,const point3& origins, const vec3& directions){
+	if (r == NULL) return 0;
+	r->origin(origins);
+	r->direction(directions);
+
+}
+
+point3 origin(ray &r) const  { return r->orig; }
+vec3 direction(ray &r) const { return r->dir; }
+point3 at(ray &r, double t) const {
+	return r->orig + t*(r->dir);
+}
+#endif
