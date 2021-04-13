b9da7af3ee6393ffd4dfbb9836da1c29a9fee180
Iago Caran Aquino <iago.caran@gmail.com>
2021-04-13 11:48:47 +0200

    Update vec3 naming and pointer usage

--- vec3.c
+++ vec3.c
@@ -1,157 +1,141 @@
 #include "vec3.h"
 
-#ifndef RAND_MAX
-#define RAND_MAX ((int)((unsigned)~0 >> 1))
-#endif
-
-double random_double(double low, double high) {
-  double d;
-
-  d = (double) rand() / ((double)RAND_MAX + 1);
-  return (low + d * (high - low));
+inline vec3 create_vec3(double x, double y, double z) {
+    vec3 vec;
+    vec.x = x;
+    vec.y = y;
+    vec.z = z;
+    return vec;
 }
 
-vec3 * create_vec3(double x, double y, double z) {
-  vec3 * vec = malloc(sizeof(struct vec3));
-  vec->x = x;
-  vec->y = y;
-  vec->z = z;
-  return vec;
+inline vec3 clone_vec3(vec3 vec) {
+    return create_vec3(vec.x, vec.y, vec.z);
 }
 
-vec3 * clone_vec3(vec3 * vec) { return create_vec3(vec->x, vec->y, vec->z); }
-
-vec3 * create_random_interval_vec3(double low, double high) {
-  return create_vec3(random_double(low, high), random_double(low, high),
-                     random_double(low, high));
+inline vec3 create_random_interval_vec3(double low, double high) {
+    return create_vec3(random_double(low, high), random_double(low, high),
+                                         random_double(low, high));
 }
 
-vec3 * create_random_vec3() { return create_random_interval_vec3(-100, 100); }
-
-void invert_vec3(vec3 * vec) {
-  vec->x = -vec->x;
-  vec->y = -vec->y;
-  vec->z = -vec->z;
+inline vec3 create_random_vec3() {
+    return create_random_interval_vec3(-100, 100);
 }
 
-void add_vec3(vec3 * left, vec3 * right) {
-  left->x = left->x + right->x;
-  left->y = left->y + right->y;
-  left->z = left->z + right->z;
+inline vec3 invert_vec3(vec3 vec) {
+    vec3 out;
+    out.x = -vec.x;
+    out.y = -vec.y;
+    out.z = -vec.z;
+    return out;
 }
 
-void add_scalar_vec3(vec3 * vec, double scalar) {
-  vec->x = vec->x + scalar;
-  vec->y = vec->y + scalar;
-  vec->z = vec->z + scalar;
+inline vec3 add_vec3(vec3 left, vec3 right) {
+    vec3 out;
+    out.x = left.x + right.x;
+    out.y = left.y + right.y;
+    out.z = left.z + right.z;
+    return out;
 }
 
-void subtract_vec3(vec3 * left, vec3 * right) {
-  left->x = left->x - right->x;
-  left->y = left->y - right->y;
-  left->z = left->z - right->z;
+inline vec3 add_scalar_vec3(vec3 vec, double scalar) {
+    vec3 out;
+    out.x = vec.x + scalar;
+    out.y = vec.y + scalar;
+    out.z = vec.z + scalar;
+    return out;
 }
 
-void subtract_scalar_vec3(vec3 * vec, double scalar) {
-  add_scalar_vec3(vec, -scalar);
+inline vec3 subtract_vec3(vec3 left, vec3 right) {
+    vec3 out;
+    out.x = left.x - right.x;
+    out.y = left.y - right.y;
+    out.z = left.z - right.z;
+    return out;
 }
 
-double dot_product_vec3(vec3 * vec1, vec3 * vec2) {
-  return vec1->x * vec2->x + vec1->y * vec2->y + vec1->z * vec2->z;
+inline vec3 subtract_vec3_d(vec3 vec, double scalar) {
+    return add_scalar_vec3(vec, -scalar);
 }
 
-void cross_product_vec3(vec3 * left, vec3 * right) {
-  double left_x = left->x;
-  double left_y = left->y;
-  double left_z = left->z;
-
-  left->x = left_y * right->z - left_z * right->y;
-  left->y = left_z * right->x - left_x * right->z;
-  left->z = left_x * right->y - left_y * right->x;
+inline double dot_product_vec3(vec3 vec1, vec3 vec2) {
+    return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
 }
 
-void multiply_scalar_vec3(vec3 * vec, double scalar) {
-  vec->x = vec->x * scalar;
-  vec->y = vec->y * scalar;
-  vec->z = vec->z * scalar;
+inline vec3 cross_product_vec3(vec3 left, vec3 right) {
+    vec3 out;
+
+    out.x = left.y * right.z - left.z * right.y;
+    out.y = left.z * right.x - left.x * right.z;
+    out.z = left.x * right.y - left.y * right.x;
+
+    return out;
 }
 
-void divide_scalar_vec3(vec3 * vec, double scalar) {
-  return multiply_scalar_vec3(vec, 1 / scalar);
+inline vec3 multiply_vec3_d(vec3 vec, double scalar) {
+    vec3 out;
+    out.x = vec.x * scalar;
+    out.y = vec.y * scalar;
+    out.z = vec.z * scalar;
+    return out;
 }
 
-double length_squared_vec3(vec3 * vec) { return dot_product_vec3(vec, vec); }
-
-double length_vec3(vec3 * vec) { return sqrt(length_squared_vec3(vec)); }
-
-int is_near_zero_vec3(vec3 * vec) {
-  const double s = 1e-8;
-  return fabs(vec->x) < s && fabs(vec->y) < s && (vec->z) < s;
+inline vec3 divide_vec3_d(vec3 vec, double scalar) {
+    return multiply_vec3_d(vec, 1 / scalar);
 }
 
-void unit_vector_vec3(vec3 * vec) { divide_scalar_vec3(vec, length_vec3(vec)); }
+inline double length_squared_vec3(vec3 vec) { return dot_product_vec3(vec, vec); }
 
-vec3 * random_in_unit_sphere() {
-  vec3 * vec = create_random_interval_vec3(-1, 1);
-  while (length_squared_vec3(vec) >= 1) {
-    free(vec);
-    vec = create_random_interval_vec3(-1, 1);
-  }
-  return vec;
+inline double length_vec3(vec3 vec) { return sqrt(length_squared_vec3(vec)); }
+
+int is_near_zero_vec3(vec3 vec) {
+    const double s = 1e-8;
+    return fabs(vec.x) < s && fabs(vec.y) < s && (vec.z) < s;
 }
 
-vec3 * random_unit_vector_vec3() {
-  vec3 * vec = random_in_unit_sphere();
-  unit_vector_vec3(vec);
-  return vec;
+inline vec3 unit_vector_vec3(vec3 vec) {
+    return divide_vec3_d(vec, length_vec3(vec));
 }
 
-vec3 * random_in_hemisphere_vec3(vec3 * normal) {
-  vec3 * in_unit_sphere = random_in_unit_sphere();
-  double prod = dot_product_vec3(in_unit_sphere, normal);
-  if (prod <= 0.0) {
-    invert_vec3(in_unit_sphere);
-  }
-  return in_unit_sphere;
+vec3 random_in_unit_sphere() {
+    vec3 vec = create_random_interval_vec3(-1, 1);
+    while (length_squared_vec3(vec) >= 1) {
+        vec = create_random_interval_vec3(-1, 1);
+    }
+    return vec;
 }
 
-vec3 * random_in_unit_disk_vec3() {
-  vec3 * vec = create_random_interval_vec3(-1, 1);
-  vec->z = 0;
-  while (length_squared_vec3(vec) >= 1) {
-    free(vec);
-    vec = create_random_interval_vec3(-1, 1);
-    vec->z = 0;
-  }
-  return vec;
+vec3 random_unit_vector_vec3() {
+    vec3 vec = random_in_unit_sphere();
+    unit_vector_vec3(vec);
+    return vec;
 }
 
-void reflect_vec3(vec3 * vec, vec3 * normal) {
-  vec3 * aux_normal = clone_vec3(normal);
-
-  double dp = 2 * dot_product_vec3(vec, normal);
-  multiply_scalar_vec3(aux_normal, dp);
-  subtract_vec3(vec, aux_normal);
-
-  free(aux_normal);
+vec3 random_in_hemisphere_vec3(vec3 normal) {
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    double prod = dot_product_vec3(in_unit_sphere, normal);
+    if (prod <= 0.0) {
+        invert_vec3(in_unit_sphere);
+    }
+    return in_unit_sphere;
 }
 
-vec3 * refract_vec3(vec3 * unit_vec, vec3 * normal, double etai_over_etat) {
-  vec3 * inverse_unit_vec = clone_vec3(unit_vec);
-  vec3 * perpendicular_vec = clone_vec3(normal);
-  vec3 * parallel_vec = clone_vec3(normal);
-  invert_vec3(inverse_unit_vec);
+vec3 random_in_unit_disk_vec3() {
+    vec3 vec = create_random_interval_vec3(-1, 1);
+    vec.z = 0;
+    while (length_squared_vec3(vec) >= 1) {
+        vec = create_random_interval_vec3(-1, 1);
+        vec.z = 0;
+    }
+    return vec;
+}
 
-  double cos_theta = fmin(dot_product_vec3(inverse_unit_vec, normal), 1.0);
-  multiply_scalar_vec3(perpendicular_vec, cos_theta);
-  add_vec3(perpendicular_vec, unit_vec);
-  multiply_scalar_vec3(perpendicular_vec, etai_over_etat);
+vec3 reflect_vec3(vec3 vec, vec3 normal) {
+    return subtract_vec3(vec, multiply_vec3_d(normal, 2 * dot_product_vec3(vec, normal)));
+}
 
-  multiply_scalar_vec3(
-      parallel_vec, -sqrt(fabs(1.0 - length_squared_vec3(perpendicular_vec))));
-  add_vec3(parallel_vec, perpendicular_vec);
-
-  free(inverse_unit_vec);
-  free(perpendicular_vec);
-  return parallel_vec;
+vec3 refract_vec3(vec3 unit_vec, vec3 normal, double etai_over_etat) {
+    double cos_theta = fmin(dot_product_vec3(invert_vec3(unit_vec), normal), 1);
+    vec3 r_out_perp = multiply_vec3_d(add_vec3(unit_vec, multiply_vec3_d(normal, cos_theta)), etai_over_etat);
+    vec3 r_out_parallel = multiply_vec3_d(normal, - sqrt(fabs(1.0 - length_squared_vec3(r_out_perp))));
+    return add_vec3(r_out_perp, r_out_parallel);
 }
--- vec3.h
+++ vec3.h
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "rtweekend.h"
 
 typedef struct vec3 {
   double x;
@@ -14,46 +15,46 @@ typedef struct vec3 {
 typedef struct vec3 point3;
 typedef struct vec3 color;
 
-vec3 * create_vec3(double x, double y, double z);
+vec3 create_vec3(double x, double y, double z);
 
-vec3 * clone_vec3(vec3 * vec);
+vec3 clone_vec3(vec3 vec);
 
-vec3 * create_random_vec3();
+vec3 create_random_vec3();
 
-vec3 * create_random_interval_vec3(double low, double high);
+vec3 create_random_interval_vec3(double low, double high);
 
-void invert_vec3(vec3 * vec);
+inline vec3 invert_vec3(vec3 vec);
 
-void add_vec3(vec3 * left, vec3 * right);
+inline vec3 add_vec3(vec3 left, vec3 right);
 
-void add_scalar_vec3(vec3 * vec, double scalar);
+inline vec3 add_vec3_d(vec3 vec, double scalar);
 
-void subtract_vec3(vec3 * left, vec3 * right);
+vec3 subtract_vec3(vec3 left, vec3 right);
 
-void subtract_scalar_vec3(vec3 * vec, double scalar);
+vec3 subtract_vec3_d(vec3 vec, double scalar);
 
-double dot_product_vec3(vec3 * vec1, vec3 * vec2);
+double dot_product_vec3(vec3 vec1, vec3 vec2);
 
-void cross_product_vec3(vec3 * left, vec3 * right);
+vec3 cross_product_vec3(vec3 left, vec3 right);
 
-void multiply_scalar_vec3(vec3 * vec, double scalar);
+inline vec3 multiply_vec3_d(vec3 vec, double scalar);
 
-void divide_scalar_vec3(vec3 * vec, double scalar);
+inline vec3 divide_vec3_d(vec3 vec, double scalar);
 
-double length_vec3(vec3 * vec);
+double length_vec3(vec3 vec);
 
-int is_near_zero_vec3(vec3 * vec);
+int is_near_zero_vec3(vec3 vec);
 
-void unit_vector_vec3(vec3 * vec);
+inline vec3 unit_vector_vec3(vec3 vec);
 
-vec3 * random_unit_vector_vec3();
+vec3 random_unit_vector_vec3();
 
-vec3 * random_in_hemisphere_vec3(vec3 * normal);
+vec3 random_in_hemisphere_vec3(vec3 normal);
 
-vec3 * random_in_unit_disk_vec3();
+vec3 random_in_unit_disk_vec3();
 
-void reflect_vec3(vec3 * vec, vec3 * normal);
+vec3 reflect_vec3(vec3 vec, vec3 normal);
 
-vec3 * refract_vec3(vec3 * unit_vec, vec3 * normal, double etai_over_etat);
+vec3 refract_vec3(vec3 unit_vec, vec3 normal, double etai_over_etat);
 
 #endif  // RAYTRACING_VEC3_H
