4e33f09caa9ba157f9fd3fc88323ad5194018fd4
Iago Caran Aquino <iago.caran@gmail.com>
2021-04-13 11:49:56 +0200

    Update ray.h for new vec3 naming

--- ray.h
+++ ray.h
@@ -1,27 +1,21 @@
-#ifndef RAY_H
-#define RAY_H
+#ifndef RAYTRACING_RAY_H
+#define RAYTRACING_RAY_H
 
 #include "vec3.h"
 
 typedef struct ray {
-	point3 orig;
-        vec3 dir;
+    point3 origin;
+    vec3 direction;
+} ray;
 
-        void (*ray)();
-        void (*ray)(ray,point3,vec3);
-        point3 (*at_ptr)(ray,double);
-};
-void ray() {}
-
-void ray(ray &r,const point3& origins, const vec3& directions){
-	if (r == NULL) return 0;
-	r->orig = origins;
-	r->dir = directions;
-	r->at_ptr = at;
+ray create_ray(const point3 * origins, const vec3 * directions){
+    ray r;
+    r.origin = clone_vec3(*origins);
+    r.direction = clone_vec3(*directions);
 }
 
-point3 at(ray &r, double t) const {
-	return r->orig + t*(r->dir);
+point3 at(ray *r, double t) {
+    return add_vec3(r->origin, multiply_vec3_d(r->direction, t));
 }
-#endif
 
+#endif //RAYTRACING_RAY_H
