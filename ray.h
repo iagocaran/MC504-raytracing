e06e84dc66e1c8a1ff90d35ef380d7da4394224a
Iago Caran Aquino <iago.caran@gmail.com>
2021-04-13 13:23:19 +0200

    Separate ray declaration and definition

--- ray.h
+++ ray.h
@@ -8,14 +8,8 @@ typedef struct ray {
     vec3 direction;
 } ray;
 
-ray create_ray(const point3 * origins, const vec3 * directions){
-    ray r;
-    r.origin = clone_vec3(*origins);
-    r.direction = clone_vec3(*directions);
-}
+ray create_ray(point3 origins, vec3 directions);
 
-point3 at(ray *r, double t) {
-    return add_vec3(r->origin, multiply_vec3_d(r->direction, t));
-}
+point3 at(ray *r, double t);
 
 #endif //RAYTRACING_RAY_H
